f0cc117748484661ef5f29cfaba5acfba8e9621a
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-08-11 11:35:52 -0500

    Removed Duplicate Declarations

--- Sources/Axis/Views/Line.swift
+++ Sources/Axis/Views/Line.swift
@@ -1,65 +1,65 @@
+////
+////  Line.swift
+////  
+////
+////  Created by Dr. Brandon Wiley on 8/4/20.
+////
 //
-//  Line.swift
-//  
+//import Foundation
+//import SwiftUI
 //
-//  Created by Dr. Brandon Wiley on 8/4/20.
+//struct LineChartLinesAndPoints: View
+//{
+//    let pointset: PointSet
+//    
+//    var body: some View
+//    {
+//        ZStack
+//        {
+//            LineChartLines(pointset: pointset)
+//            LineChartPoints(pointset: pointset)
+//        }
+//    }
+//}
 //
-
-import Foundation
-import SwiftUI
-
-struct LineChartLinesAndPoints: View
-{
-    let pointset: PointSet
-    
-    var body: some View
-    {
-        ZStack
-        {
-            LineChartLines(pointset: pointset)
-            LineChartPoints(pointset: pointset)
-        }
-    }
-}
-
-struct LineChartLines: View
-{
-    let pointset: PointSet
-    
-    var body: some View
-    {
-        Path
-        {
-            path in
-            
-            path.move(to: pointset.points.first!.cgpoint)
-
-            for point in pointset.points[1...]
-            {
-                path.addLine(to: point.cgpoint)
-            }
-        }
-        .stroke(pointset.color, lineWidth: 10)
-    }
-}
-
-struct LineChartPoints: View
-{
-    let pointset: PointSet
-    
-    var body: some View
-    {
-        ForEach(pointset.points, id: \.id)
-        {
-            point in
-            
-            point
-        }
-    }
-}
-
-struct Line_Previews: PreviewProvider {
-    static var previews: some View {
-        LineChartLinesAndPoints(pointset: PointSet(id: UUID().uuidString, label: "test", color: Color.red, points: [Point(x: 0, y: 0)]))
-    }
-}
+//struct LineChartLines: View
+//{
+//    let pointset: PointSet
+//    
+//    var body: some View
+//    {
+//        Path
+//        {
+//            path in
+//            
+//            path.move(to: pointset.points.first!.cgpoint)
+//
+//            for point in pointset.points[1...]
+//            {
+//                path.addLine(to: point.cgpoint)
+//            }
+//        }
+//        .stroke(pointset.color, lineWidth: 10)
+//    }
+//}
+//
+//struct LineChartPoints: View
+//{
+//    let pointset: PointSet
+//    
+//    var body: some View
+//    {
+//        ForEach(pointset.points, id: \.id)
+//        {
+//            point in
+//            
+//            point
+//        }
+//    }
+//}
+//
+//struct Line_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LineChartLinesAndPoints(pointset: PointSet(id: UUID().uuidString, label: "test", color: Color.red, points: [Point(x: 0, y: 0)]))
+//    }
+//}
